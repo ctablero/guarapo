406a9bb9c1d09d456a09b4e84a49c3c9eb44b6e3
Cesar Tablero <cesarjthernandez@gmail.com>
2024-09-08 00:29:10 -0400

    gpo-1: Usage of results variable. Validation in case there are not results.

--- analyzer.sh
+++ analyzer.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-#Retrieve required parameters
+# Retrieve required parameters
 threshold_pcent=$1
 
 # Get mounted filesystems that are higher the threshold
-df -h | awk -v pcent="$threshold_pcent" '{ if($5+0 >= pcent) print $0 }'
+results="$(df -h | awk -v pcent="$threshold_pcent" '{ if($5+0 >= pcent) print $0 }')"
+
+if [ -n "$results" ]
+then
+  printf "%s\n" "$results"
+
+# Here you will use $results variable and send to the alert channel of preference. 
+# It can be Nagios, Pagerduty, notification-send etc
+
+else
+  printf "%s\n" "None high disk usage detected."  
+fi
+
+# End message
+printf "%s\n" "Analysis completed."

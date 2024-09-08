f15ea49ddd3f89fbfbf22313fd15095d1ef4b721
Cesar Tablero <cesarjthernandez@gmail.com>
2024-09-08 12:37:35 -0400

    gpo-1: Applied single responsability principle and made the script only in charge of the analysis

--- analyzer.sh
+++ analyzer.sh
@@ -6,16 +6,12 @@ threshold_pcent=$1
 # Get mounted filesystems that are higher the threshold
 results="$(df -h | awk -v pcent="$threshold_pcent" '{ if($5+0 >= pcent) print $0 }')"
 
+# Manage of the output
 if [ -n "$results" ]
 then
   printf "%s\n" "$results"
 
-# Here you will use $results variable and send to the alert channel of preference. 
-# It can be Nagios, Pagerduty, notification-send etc
-
 else
-  printf "%s\n" "None high disk usage detected."  
+  printf "%s\n" "None high disk usage detected."
+  exit 1
 fi
-
-# End message
-printf "%s\n" "Analysis completed."
